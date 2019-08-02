92de8fba8250175dd0c16d21e56eb7480264e9d1
Nick-Hericks <nickhericks@gmail.com>
2019-08-02 08:46:38 +0200

    Fix JS styling issues

--- js/main.js
+++ js/main.js
@@ -1,63 +1,10 @@
+// Select
+const hand = document.querySelector('.wave');
 
-
-const wave = document.querySelector('.wave');
-
-wave.addEventListener('mouseenter', () => {
-	// wave.classList.add('wave');
-
-	wave.style.animationIterationCount = 'infinite';
-
+hand.addEventListener('mouseenter', () => {
+	hand.style.animationIterationCount = 'infinite';
 });
 
-wave.addEventListener('mouseleave', () => {
-	// wave.classList.remove('wave');
-
-	wave.style.animationIterationCount = "0";
-
-
+hand.addEventListener('mouseleave', () => {
+	hand.style.animationIterationCount = '0';
 });
-
-
-
-// const yellow = document.querySelector("#yellow");
-
-
-// yellow.addEventListener("mouseenter", () => {
-// 	yellow.style.display = 'none';
-//   yellow.animation-iteration-count = 'infinite';
-// });
-
-
-
-
-
-
-// const container = document.querySelector(".container");
-
-
-
-// // Wave hand
-// const wave = () => {
-// 	const hand = document.querySelector('.big-hand');
-
-
-	
-// 	const tl = new TimelineMax({});
-// 	// tl.set(hand, { transformOrigin: 'bottom center' });
-// 	// tl.from(hand, 0.5, {
-// 	// 	scale: 0.5,
-// 	// 	opacity: 0,
-// 	// 	ease: Back.easeOut.config(1.5)
-// 	// });
-// 	tl.to(hand, 0.2, { rotation: 15 });
-// 	tl.to(hand, 0.2, { rotation: -15 });
-// 	tl.to(hand, 0.2, { rotation: 15 });
-// 	tl.to(hand, 0.2, { rotation: -15 });
-// 	tl.to(hand, 0.2, { rotation: 0 });
-// };
-
-
-
-// container.addEventListener('click', () => {
-// 	wave();
-// });
