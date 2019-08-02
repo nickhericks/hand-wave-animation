15a008fc7b10f121fbc099b14be224ff140fc3ad
Nick-Hericks <nickhericks@gmail.com>
2019-08-02 09:14:22 +0200

    Remove JS and use CSS :hover

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-// Select
-const hand = document.querySelector('.wave');
+// Select hand emoji
+// const hand = document.querySelector('.wave');
 
-hand.addEventListener('mouseenter', () => {
-	hand.style.animationIterationCount = 'infinite';
-});
 
-hand.addEventListener('mouseleave', () => {
-	hand.style.animationIterationCount = '0';
-});
+// hand.addEventListener('mouseenter', () => {
+// 	hand.style.animationTimingFunction = 'ease-in-out';
+// 	hand.style.animationIterationCount = 'infinite';
+// });
+
+// hand.addEventListener('mouseleave', () => {
+// 	hand.style.animationIterationCount = '0';
+// });
